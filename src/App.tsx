e2174eab480caed66a0379fe8fc3982c37d92e15
Tony Peng <lp1dxing@gmail.com>
2022-05-10 01:02:44 +1000

    made code more reusable

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,15 @@ const inputStyleHaveWidth = { height: '3em', backgroundColor: 'rgba(255,255,255,
 const inputStyleNoHeight = { backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em' }
 const errorOutlineIconStyle = { color: 'red', fontSize: '1em' };
 const stateOptions = ["New South Wales", "Victoria", "Queensland", "Western Australia", "South Australia", "Tasmania"]
+
 const isEmail = (str: string): boolean => {
   var reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
   return reg.test(str);
 }
 
 function App() {
+  const [emailOptOut, setEmailOptOut] = useState(false)
+
   const [formData, setFormData] = useState({
     gender: '',
     firstName: '',
@@ -37,7 +40,7 @@ function App() {
     fax: '',
     title: '',
     email: '',
-    emailOptOut: "no",
+    emailOptOut,
     streetNo: '',
     city: '',
     state: '',
@@ -64,117 +67,79 @@ function App() {
   const [descriptionError, setDescriptionError] = useState('')
   const [openDialog, setOpenDialog] = useState(false)
 
-  const changeFiled = (fieldName: string, value: string) => {
-    //change variables in state, force app to rerender when change Field
-    const newFormData = JSON.parse(JSON.stringify(formData));
-    type formDataKeyType = keyof typeof newFormData;
-    let fieldNameKey = fieldName as formDataKeyType
-    newFormData[fieldNameKey] = value;
-    setFormData(newFormData)
-  }
-  const changeGender = (event: SelectChangeEvent) => {
-    const value = event.target.value;
-    if (value) {
-      setGenderError('')
+
+
+  const changeField = (event: React.ChangeEvent<HTMLInputElement> | any) => {
+    let name = event.target.name
+    let val = event.target.value
+    if (val) {
+      if (name === 'gender') {
+        setGenderError('')
+      }
+
+      if (name === 'firstName') {
+        setFirstNameError('')
+      }
+
+      if (name === 'lastName') {
+        setLastNameError('')
+      }
+
+      if (name === 'accountName') {
+        setAccountNameError('')
+      }
+
+      if (name === 'email') {
+        if (!isEmail(val) && val !== '') {
+          setEmailError('Please input valid email')
+        } else {
+          setEmailError('')
+          setEmailErrorMsg('')
+        }
+      }
+
+      if (name === 'emailOptOut') {
+        setEmailOptOut(!emailOptOut)
+      }
+
+      if (name === 'streetNo') {
+        setStreetNoError('')
+      }
     }
-    changeFiled("gender", value)
+
+    setFormData({
+      ...formData,
+      [name]: val
+    })
   }
-  const changeFirstName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (value) {
-      setFirstNameError('')
-    }
-    changeFiled("firstName", value)
-  }
-  const changeLastName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (value) {
-      setLastNameError('')
-    }
-    changeFiled("lastName", value)
-  }
-  const changeAccountName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (value) {
-      setAccountNameError('')
-    }
-    changeFiled("accountName", event.target.value)
-  }
-  const changeCompanyName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    changeFiled("companyName", event.target.value)
-  }
-  const changePhone = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value.replace(/\D/g, '');
-    if (value.length !== 10 && value !== '') {
-      setPhoneError('phone length must be 10!')
+
+  const changePhoneHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //avoid typing non-digit
+    const val = e.target.value.replace(/\D/g, '')
+    //valid AU numbers
+    if (val.length !== 10 && val !== '') {
+      setPhoneError('phone number length must be 10 digits')
     } else {
       setPhoneError('')
       setPhoneErrorMsg('')
     }
-    changeFiled("phone", value)
+
+    setFormData({
+      ...formData,
+      [e.target.name]: val
+    })
   }
-  const changeFax = (event: React.ChangeEvent<HTMLInputElement>) => {
-    changeFiled("fax", event.target.value)
-  }
-  const changeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    changeFiled("title", event.target.value)
-  }
-  const changeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (!isEmail(value) && value !== '') {
-      setEmailError("pls input correct email")
-    } else {
-      setEmailError('')
-      setEmailErrorMsg('')
-    }
-    changeFiled("email", value)
-  }
-  const emailOptOutChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      changeFiled("emailOptOut", 'yes')
-    } else {
-      changeFiled("emailOptOut", 'no')
-    }
-  }
-  const changeStreetNo = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (value) {
-      setStreetNoError('')
-    }
-    changeFiled("streetNo", event.target.value)
-  }
-  const changeCity = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (value) {
-      setCityError('')
-    }
-    changeFiled("city", event.target.value)
-  }
-  const changeState = (event: any, newValue: string | null) => {
-    const value = newValue;
-    if (value) {
+
+  const changeStateHandler = (e: any, newValue: string | null) => {
+    if (newValue) {
       setStateError('')
     }
-    changeFiled("state", value ? value : '')
+    setFormData({
+      ...formData,
+      state: newValue || ''
+    })
   }
-  const changePostcode = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //matches all non digit character
-    const value = event.target.value.replace(/\D/g, '');
-    if (value.length !== 4 && value !== '') {
-      setPostCodeError('post code length must be 4!')
-    } else {
-      setPostCodeError('')
-      setPostCodeErrorMsg('')
-    }
-    changeFiled("postcode", value)
-  }
-  const changeDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (value) {
-      setDescriptionError('')
-    }
-    changeFiled("description", value)
-  }
+
   const handDialog = () => {
     setOpenDialog(false)
   }
@@ -189,7 +154,7 @@ function App() {
       fax: '',
       title: '',
       email: '',
-      emailOptOut: "no",
+      emailOptOut: false,
       streetNo: '',
       city: '',
       state: '',
@@ -201,25 +166,27 @@ function App() {
   }
   const doSave = () => {
     if (!formData.gender) {
-      setGenderError('pls select gender!');
+      setGenderError('Please select gender!');
       return;
     }
+
     if (!formData.firstName) {
-      setFirstNameError('pls input First Name!');
+      setFirstNameError('Please input First Name!');
       return;
     }
+
     if (!formData.lastName) {
-      setLastNameError('pls input Last Name!');
+      setLastNameError('Please input Last Name!');
       return;
     }
 
     if (!formData.accountName) {
-      setAccountNameError('pls input Account Name!');
+      setAccountNameError('Please input Account Name!');
       return;
     }
 
     if (!formData.phone) {
-      setPhoneError('pls input phone!');
+      setPhoneError('please input phone number!');
       return;
     }
 
@@ -228,7 +195,7 @@ function App() {
     }
 
     if (!formData.email) {
-      setEmailError('pls input email!');
+      setEmailError('Please input email!');
       return;
     }
 
@@ -237,27 +204,27 @@ function App() {
     }
 
     if (!formData.streetNo) {
-      setStreetNoError('pls input Street No. & Street!');
+      setStreetNoError('Please input Street No. & Street!');
       return;
     }
 
     if (!formData.city) {
-      setCityError('pls input City!');
+      setCityError('Please input City!');
       return;
     }
 
     if (!formData.state) {
-      setStateError('pls select state!');
+      setStateError('Please select state!');
       return;
     }
 
     if (!formData.postcode) {
-      setStreetNoError('pls input Postcode!');
+      setStreetNoError('Please input Postcode!');
       return;
     }
 
     if (!formData.description) {
-      setDescriptionError('pls input Description!');
+      setDescriptionError('Please input Description!');
       return;
     }
 
@@ -267,6 +234,7 @@ function App() {
 
     setOpenDialog(true)
   }
+
   return (
     <div className="myapp">
       <header className="header">
@@ -300,9 +268,10 @@ function App() {
                 <label>First Name</label>
                 <div className="form-flex">
                   <Select
+                    name='gender'
                     value={formData.gender}
                     sx={{ ...inputStyleAutoWidth, '& fieldSet': { borderWidth: '1px', borderColor: genderError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
-                    onChange={changeGender}
+                    onChange={changeField}
                     displayEmpty
                     inputProps={{ 'aria-label': 'Without label' }}
                   >
@@ -313,8 +282,9 @@ function App() {
                     <MenuItem value={"Mrs"}>Mrs</MenuItem>
                   </Select>
                   <OutlinedInput
+                    name='firstName'
                     value={formData.firstName}
-                    onChange={changeFirstName}
+                    onChange={changeField}
                     sx={{ ...inputStyleAutoWidth, flex: '2', '& fieldSet': { borderWidth: '1px', borderColor: firstNameError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
                     placeholder="John"
                   />
@@ -324,9 +294,10 @@ function App() {
                 <label>Last Name</label>
                 <div>
                   <OutlinedInput
+                    name='lastName'
                     sx={{ ...inputStyleHaveWidth, '& fieldSet': { borderWidth: '1px', borderColor: lastNameError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
                     value={formData.lastName}
-                    onChange={changeLastName}
+                    onChange={changeField}
                     placeholder="Smith"
                   />
                 </div>
@@ -338,8 +309,9 @@ function App() {
                 <label>Account Name</label>
                 <div>
                   <OutlinedInput
+                    name='accountName'
                     value={formData.accountName}
-                    onChange={changeAccountName}
+                    onChange={changeField}
                     sx={{ ...inputStyleHaveWidth, '& fieldSet': { borderWidth: '1px', borderColor: accountNameError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
                     placeholder="John's Joinery"
                   />
@@ -349,8 +321,9 @@ function App() {
                 <label>Company Name(optional)</label>
                 <div>
                   <OutlinedInput
+                    name='companyName'
                     value={formData.companyName}
-                    onChange={changeCompanyName}
+                    onChange={changeField}
                     sx={inputStyleHaveWidth}
                     placeholder=""
                   />
@@ -363,11 +336,18 @@ function App() {
                 <label>Phone</label>
                 <div className="form-have-tips">
                   <TextField
+                    name='phone'
                     label=""
                     value={formData.phone}
-                    onChange={changePhone}
+                    onChange={changePhoneHandler}
                     helperText={phoneErrorMsg}
-                    sx={{ '& fieldSet': { borderWidth: '1px', borderColor: phoneError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' }, display: 'block', '& p': { lineHeight: '1em', color: 'red' }, '& .MuiOutlinedInput-root': { width: '100%', height: '3.8em', fontSize: '0.8em' }, height: '3em', backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em' }}
+                    sx={{
+                      '& fieldSet':
+                      {
+                        borderWidth: '1px',
+                        borderColor: phoneError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);'
+                      }, display: 'block', '& p': { lineHeight: '1em', color: 'red' }, '& .MuiOutlinedInput-root': { width: '100%', height: '3.8em', fontSize: '0.8em' }, height: '3em', backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em'
+                    }}
                     placeholder="02 123 456 78"
                     variant="outlined"
                     size="small"
@@ -391,8 +371,9 @@ function App() {
                 <label>Fax(optional)</label>
                 <div>
                   <OutlinedInput
+                    name='fax'
                     value={formData.fax}
-                    onChange={changeFax}
+                    onChange={changeField}
                     sx={inputStyleHaveWidth}
                     placeholder="John's Joinery"
                   />
@@ -405,8 +386,9 @@ function App() {
                 <label>Title(optional)</label>
                 <div>
                   <OutlinedInput
+                    name='title'
                     value={formData.title}
-                    onChange={changeTitle}
+                    onChange={changeField}
                     sx={inputStyleHaveWidth}
                     placeholder="Owner"
                   />
@@ -416,12 +398,19 @@ function App() {
                 <label>Email</label>
                 <div className="form-have-tips">
                   <TextField
+                    name='email'
                     label=""
                     value={formData.email}
                     helperText={emailErrorMsg}
-                    onChange={changeEmail}
-                    sx={{ '& fieldSet': { borderWidth: '1px', borderColor: emailError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' }, display: 'block', '& p': { lineHeight: '1em', color: 'red' }, '& .MuiOutlinedInput-root': { width: '100%', height: '3.8em', fontSize: '0.8em' }, height: '3em', backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em' }}
-                    placeholder="samle@email.com"
+                    onChange={changeField}
+                    sx={{
+                      '& fieldSet':
+                      {
+                        borderWidth: '1px',
+                        borderColor: emailError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);'
+                      }, display: 'block', '& p': { lineHeight: '1em', color: 'red' }, '& .MuiOutlinedInput-root': { width: '100%', height: '3.8em', fontSize: '0.8em' }, height: '3em', backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em'
+                    }}
+                    placeholder="sample@email.com"
                   />
                   {
                     emailError ?
@@ -443,8 +432,9 @@ function App() {
               <div className="form-half show-flex">
                 <label className="inline">Email Opt Out</label>
                 <Checkbox
-                  value={formData.emailOptOut == 'yes' ? true : false}
-                  onChange={emailOptOutChange}
+                  name='emailOptOut'
+                  value={formData.emailOptOut}
+                  onChange={changeField}
                   inputProps={{ 'aria-label': 'controlled' }}
                 />
               </div>
@@ -461,9 +451,10 @@ function App() {
                 <label>Street No. & Street</label>
                 <div>
                   <OutlinedInput
+                    name='streetNo'
                     sx={{ ...inputStyleHaveWidth, '& fieldSet': { borderWidth: '1px', borderColor: streetNoError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
                     value={formData.streetNo}
-                    onChange={changeStreetNo}
+                    onChange={changeField}
                     placeholder="1, Elizabeth Street"
                   />
                 </div>
@@ -472,9 +463,10 @@ function App() {
                 <label>City</label>
                 <div>
                   <OutlinedInput
+                    name='city'
                     sx={{ ...inputStyleHaveWidth, '& fieldSet': { borderWidth: '1px', borderColor: cityError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
                     value={formData.city}
-                    onChange={changeCity}
+                    onChange={changeField}
                     placeholder="Sydney"
                   />
                 </div>
@@ -487,7 +479,7 @@ function App() {
                   <Autocomplete
                     value={formData.state}
                     sx={{ '& .MuiAutocomplete-endAdornment': { top: '0' }, '& .MuiOutlinedInput-root': { width: '100%', height: '3.8em', paddingTop: '0', paddingBottom: '0', lineHeight: '3.8em', fontSize: '0.8em' }, height: '3em', backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em' }}
-                    onChange={changeState}
+                    onChange={changeStateHandler}
                     options={stateOptions}
                     renderInput={(params) => <TextField sx={{ '& fieldSet': { borderWidth: '1px', borderColor: stateError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }} placeholder="New South Wales" {...params} />}
                   />
@@ -501,7 +493,7 @@ function App() {
                     helperText={postCodeErrorMsg}
                     sx={{ '& fieldSet': { borderWidth: '1px', borderColor: postCodeError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' }, display: 'block', '& p': { lineHeight: '1em', color: 'red' }, '& .MuiOutlinedInput-root': { width: '100%', height: '3.8em', fontSize: '0.8em' }, height: '3em', backgroundColor: 'rgba(255,255,255,1)', width: '100%', fontSize: '0.8em' }}
                     value={formData.postcode}
-                    onChange={changePostcode}
+                    // onChange={changePostcode}
                     placeholder="2000"
                   />
                   {
@@ -533,7 +525,7 @@ function App() {
                     multiline
                     value={formData.description}
                     rows={4}
-                    onChange={changeDescription}
+                    // onChange={changeDescription}
                     sx={{ ...inputStyleNoHeight, '& fieldSet': { borderWidth: '1px', borderColor: descriptionError ? 'rgba(255,0,0,0.8) !important' : 'rgba(0, 0, 0, 0.23);' } }}
                     placeholder=""
                   />
@@ -620,7 +612,7 @@ function App() {
               <div className="row-text-half">
                 <label>Email Opt Out</label>
                 <div>
-                  {formData.emailOptOut == 'yes' ? 'yes' : ''}
+                  {formData.emailOptOut}
                 </div>
               </div>
             </div>
